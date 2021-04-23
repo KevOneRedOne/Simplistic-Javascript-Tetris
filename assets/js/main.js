edb5f28e48b6af78caba7fd5898d5ec94bbea6c6
Kevin Alves <kevinalves@hotmail.fr>
2021-04-24 00:49:15 +0200

    Update Tetrisgame 24/04/21- 00:49

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,23 +91,31 @@ var p = randomPiece();
 document.addEventListener("keydown",CONTROL);
 
 function CONTROL(event){
-    if(event.keyCode == 37){
+    if(event.keyCode == 37 || event.keyCode == 81){ //keycode 37 = arrow left / 81 = Q
         p.moveLeft();
         dropStart = Date.now();
-    }else if(event.keyCode == 38){
+    }else if(event.keyCode == 38 || event.keyCode == 90){ //keycode 38 = arrow up / 90 = Z
         p.rotate();
         dropStart = Date.now();
-    }else if(event.keyCode == 39){
+    }else if(event.keyCode == 39 || event.keyCode == 68){ //keycode 39 = arrow right / 68 = D
         p.moveRight();
         dropStart = Date.now();
-    }else if(event.keyCode == 40){
+    }else if(event.keyCode == 40 || event.keyCode == 83){ // keycode 40 = arrow down / 83 = S
         p.moveDown();
+    } else if (event.keyCode == 32) { // keycode 32 = Space 
+        p.moveDown();
+    } else if (event.keyCode == 27) { // keycode 27 = Escape
+        pause();
     }
+
 }
+
 
 /*---------------------------------------------------------------------------
 --------------------------Tetrominoes Drop Down Animation--------------------
 -----------------------------------------------------------------------------*/
+
+//The static Date.now() method returns the number of milliseconds elapsed since January 1
 var dropStart = Date.now();
 var gameOver = false;
 
@@ -116,7 +124,7 @@ function drop(){
     document.querySelector('#pause-btn').style.display = 'block'
     document.querySelector('#restart-btn').style.display = 'block'
     
-    showHighScores();
+    
 
     let now = Date.now();
     let delta = now - dropStart;
@@ -130,28 +138,42 @@ function drop(){
         anime = requestAnimationFrame(drop);
     } else {
         alert('Game Over !')
-        const nameUser = prompt("Veuillez saisir votre nom : ");
-        alert('Joueur : ' + nameUser + '  '  + 'Score : '+ score);
+        var prompts = prompt("Enter your first name: ");
+        // Detect empty value on prompt
+        if (prompts == '') {
+            prompts = 'AAA'
+        };
+        // Put the answer in uppercase and select the first 3 letters
+        var nameUser = prompts.toLocaleUpperCase().substring(0,3);
+        alert(
+            'Player : ' + prompts + '\n' 
+            + 'Initials : ' + nameUser + '\n' 
+            + 'Score : '+ score
+            + '\n' + '\n'  
+            + 'Restart the game and see if you are in the High Scores !!' 
+        );
         cancelAnimationFrame(anime)
         HighScore(nameUser);
     }
+
+    showHighScores();
+
     calculateFPSNormal();
     updateLabel( FPSNormal );
 }
 
-
 /*---------------------------------------------------------------------------
------------------------------------Pause-------------------------------------
+-----------------------------------Pause function----------------------------
 -----------------------------------------------------------------------------*/
 const pause = () => {
     document.querySelector('#play-btn').style.display = 'block'
     document.querySelector('#pause-btn').style.display = 'none'
     document.querySelector('#restart-btn').style.display = 'block'
-    
-    alert('Votre partie est en pause !' + '\n' + '\n' +  'Pour relancer le jeu, cliquez sur OK, puis sur PLAY.')
-
+    alert('Your game is on pause !' 
+    + '\n' + '\n' 
+    +  'To restart the game, click "OK", then "PLAY".'
+    );
     cancelAnimationFrame(anime)
-
 }
 
 
@@ -159,20 +181,12 @@ const pause = () => {
 --------------------------------High Scores----------------------------------
 -----------------------------------------------------------------------------*/
 
- //HighScore 
+//HighScore function is used to set in local storage of the player the High Scores 
 const HighScore = (nameUser) => {
-
     const highScores= JSON.parse(localStorage.getItem("highScores")) || [] ;
-    const highScoresList = document.getElementById('highScores');
-
-
-    highScoresList.innerHTML = highScores
-        .map((Scores) => `<li> ${Scores.playername} : ${Scores.score} `)
-        .join(' ');
-
-
+    //Sets a maximum highScores
     const MAX_HIGH_SCORES = 10
-
+    //Sets an object Scores
     const Scores = {
         playername : nameUser,
         score : score 
@@ -180,18 +194,26 @@ const HighScore = (nameUser) => {
     highScores.push(Scores);
     highScores.sort((a,b) =>  b.score - a.score)
     highScores.splice(MAX_HIGH_SCORES);
+    
+    const highScoresList = document.getElementById('highScores');
 
+    highScoresList.innerHTML = highScores
+    .map((Scores) => `<li> ${Scores.playername} - ${Scores.score} `)
+    .join(' ');
+    
+
+    // Updates the local storage of the user with the JSON.stringify
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    
-    
+      
 }
 
+//showHighScores is a function that displays scores when the game is started
 const showHighScores = () => {
     const highScores= JSON.parse(localStorage.getItem("highScores")) || [] ;
     const highScoresList = document.getElementById('highScores');
 
     highScoresList.innerHTML = highScores
-        .map((Scores) => `<li> ${Scores.playername} : ${Scores.score} `)
+        .map((Scores) => `<li> ${Scores.playername} - ${Scores.score} `)
         .join(' ');
 }
 
